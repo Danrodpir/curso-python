009ce8c91a3a75b5cbcceedb34f7c4315764230e
Daniel <danny_131@hotmail.com>
2024-02-20 11:39:08 +0000

    Tema 1 - Python básico

---
+++ variablesEImports.py
@@ -0,0 +1,29 @@
+# Declaración de variables:
+x = 1
+y = "Hola mundo"
+print(x)
+print(y)
+
+# Declaracion de múltiples variables:
+edad, nombre, telefono = 22, "Carlos", 666555444
+print(edad, nombre, telefono)
+
+# Imports:
+import keyword
+keyword.kwlist
+
+# Import con alias:
+import math as mt
+print(mt.pi)
+
+# Import por modulos:
+from math import pi
+print(pi)
+
+from math import pi as numPi
+print(numPi)
+
+# Imports múltiples:
+from math import pi, log, exp
+
+
