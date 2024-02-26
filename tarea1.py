31d05a6768a13e8b1719856da53d2bac8da1478b
Daniel <danny_131@hotmail.com>
2024-02-26 11:04:28 +0000

    Tarea 1

---
+++ tarea1.py
@@ -0,0 +1,32 @@
+# Ejercicio 1: calcula la división entera de 45 entre 6.
+45 // 6  # 7
+
+# Ejercicio 2: calcula el resto de la división entera de 45 entre 6.
+45 % 6  # 3
+
+# Ejercicio 3: realiza la siguiente operación en Python, donde ÷ indica la división entera.
+# 10 + 20 ÷ 7 − 2
+10 + 20 // 7 - 2  # 10
+
+# Ejercicio 4: realiza la siguiente operación en Python (ver operación en el pdf Tarea 1 - Números en Python, ejercicio 4).
+(9 - ((25 + 5 - 2) / (7 * 4))) / 2 ** 3  # 1
+
+# Ejercicio 5: realiza la siguiente operación en Python (ver operación en el pdf Tarea 1 - Números en Python, ejercicio 5).
+(2 + 2 ** 3 - 2 * (2 - 2 ** 5) + 2 ** 2 * 2 + 2) / (2 * (2 * 2 - 2 ** 4) + 2 ** 2)  # -4
+
+# Ejercicio 6: realiza la siguiente operación en Python.
+# 6i - (4 + i) * 2
+6j - (4 + 1j) * 2  # -8+4j
+
+# Ejercicio 7: realiza la siguiente operación en Python (ver operación en el pdf Tarea 1 - Números en Python, ejercicio 7).
+((1 + 1j) / (1 - 1j)) + (2 / (-1 + 1j))  # -1+0j
+
+# Ejercicio 8: realiza la siguiente operación en Python (ver operación en el pdf Tarea 1 - Números en Python, ejercicio 8).
+pow((1 + 1j),2)  #2j
+
+# Ejercicio 8: realiza la siguiente operación en Python, donde Mod(z) indica módulo del complejo z (ver operación en el pdf Tarea 1 - Números en Python, ejercicio 8).
+abs((9 - 3j) / (-2 -1j))  # 4.242640687119285
+
+# Ejecrcicio 9: ¿Cuál es el argumento del número complejo i?
+import cmath
+cmath.phase(1j)  # 1.5707963267948966
