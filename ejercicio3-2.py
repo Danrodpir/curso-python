eb59976f9f930c05fcf96027eb5cf3703454aa8c
Daniel <danny_131@hotmail.com>
2024-03-05 12:18:18 +0000

    Ejercicio 3-2

---
+++ ejercicio3-2.py
@@ -0,0 +1,5 @@
+# Vamos a pedirle al usuario su año de nacimiento y el año actual y le vamos a imprimir por pantalla su edad:
+year = int(input("Introduce tu año de nacimiento: "))
+this_year = int(input("Introduce el año actual: "))
+age = this_year - year
+age
