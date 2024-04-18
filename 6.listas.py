25f259671f0469eb177e8c8e789d68bfc6f3bdf2
Daniel <danny_131@hotmail.com>
2024-04-18 15:02:31 +0100

    Tema 6 (incompleto)

--- 6.listas.py
+++ 6.listas.py
@@ -39,3 +39,51 @@ print(nombres)
 # Bucles con listas:
 for i in range(len(nombres)):
   print(nombres[i])
+
+for nombre in nombres:
+  print(nombre)
+
+# Concatenar listas:
+l1 = [True, 15, "perro"]
+l2 = [False, 21, "gato"]
+print(l1+l2)
+
+# Repetición de listas:
+abc = ["A", "B", "C"]
+print(abc * 5)
+
+# Listas vacias:
+empty_list = []
+print(empty_list)
+
+# .count() recibe un elemento como argumento y cuenta la cantidad de veces que aparece en la lista:
+numeros = [0, 1, 1, 2, 2, 2, 3, 3, 3]
+counted = []
+
+for element in numeros:
+  if element not in counted:
+    counted.append(element)
+    print("El elemento {} aparece {} veces".format(element, numeros.count(element)))
+
+# .extend() añade al final de la lista:
+numeros = [1, 2, 3, 4, 5]
+print(numeros)
+numeros.extend([6])
+print(numeros)
+numeros.extend([7, 8])
+print(numeros)
+numeros.extend(range(9, 16))
+print(numeros)
+
+# .index() devuelve el indice de la primera aparicion en la lista:
+numeros = [0, 1, 1, 2, 2, 2, 3, 4, 3, 4]
+print(numeros.index(2))
+print(numeros.index(4))
+
+# .pop() devuelve el último elemento de la lista y lo borra de la misma:ç
+print(numeros)
+for i in range(5):
+  print(numeros.pop())
+  print(numeros)
+
+  
